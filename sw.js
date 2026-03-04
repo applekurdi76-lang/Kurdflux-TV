a797f59933eb316928beff50867eee46720c6a61
applekurdi76-lang <applekurdi76@gmail.com>
2026-03-04 04:48:42 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'kurdflux-v1';
 const urlsToCache = [
   './',
+  'icon.png',
   'https://cdn.plyr.io/3.7.8/plyr.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/hls.js@latest',
@@ -21,7 +22,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
